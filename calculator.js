7784bbefa7d0dbfc56c7fc9532237f8bbde10d09
Frida Lindberg <fridalindberg__@hotmail.com>
2022-01-17 10:26:04 +0100

    body-parser

--- calculator.js
+++ calculator.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', (req, res) => {
+
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+
+    var result = num1 + num2;
+
+  res.send('The result of the calculation is ' + result);
 });
 
 app.listen(3000, function () {
