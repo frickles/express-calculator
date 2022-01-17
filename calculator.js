804a64ec61cf2e0354c994b7dc258f7ead932bcd
Frida Lindberg <fridalindberg__@hotmail.com>
2022-01-17 10:43:59 +0100

    bmi-calculator

--- calculator.js
+++ calculator.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}))
 
+
+//SIMPLE CALCULATOR
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
@@ -17,6 +20,24 @@ app.post('/', (req, res) => {
 
   res.send('The result of the calculation is ' + result);
 });
+
+
+//BMI CALCULATOR
+
+app.get("/bmicalculator", (req, res) => {
+    res.sendFile(__dirname + '/bmiCalculator.html')
+})
+
+app.post("/bmicalculator", (req, res) => {
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+
+    var bmi = weight / (height * height);
+
+    res.send("Your BMI is " + bmi);
+})
+
+//SERVER LISTENING
 
 app.listen(3000, function () {
   console.log('Server listening on port 3000.');
